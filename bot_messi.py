014490c19a24113462d4900b2f724ed020fd8eba
BrendaCoronel <brendacarolinacoronel9@gmail.com>
2024-08-21 15:03:40 -0300

    bot_messi

---
+++ bot_messi.py
@@ -0,0 +1,21 @@
+from bot_abstract import BotAbstract
+
+class BotGanador(BotAbstract):
+    
+    @property
+    def Nombre(self) -> str:
+        return "Ganador"
+    
+    def __init__(self):
+        self.ultima_jugada_oponente = None
+    
+    def Jugar(self, jugada_numero: int, jugada_previa_oponente: str) -> str:
+        if jugada_numero == 0:
+            # Primera jugada siempre es Murciélago
+            return 'M'
+        
+        # Estrategia basada en la jugada previa del oponente
+        if jugada_previa_oponente == 'M':
+            return 'S'  # Responder a Murciélago con Sapo
+        elif jugada_previa_oponente == 'S':
+            return 'M'  # Responder a Sapo con Murciélago
